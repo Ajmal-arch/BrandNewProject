7ee42e6599aeb57305d5a1d82c472cfbc6a1b5ca
T440s <T440s@DESKTOP-U29E12C>
2019-10-24 12:35:07 -0400

    This is the First Project Changes 0045

---
+++ DemoProject/src/demoProject1/DemoProject1.java
@@ -0,0 +1,17 @@
+package demoProject1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoProject1 {
+
+	System.setProperty(
+			"C:\\Users\\T440s\\Downloads\\CucumberSelenium (2) (2)\\cucumberSelenium\\src\\test\\resources\\drivers",
+			null);
+	driver = new ChromeDriver();
+	driver.get("https://www.zoho.com");
+    
+
+}
+
+	
+
